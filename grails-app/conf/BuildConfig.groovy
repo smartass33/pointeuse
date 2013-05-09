447f05330daea13a0e9b417b15cb869f1c895a47
smartass33 <henri.martin@gmail.com>
2013-05-09 13:54:33 +0200

    buildconfig change: prototype

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -42,15 +42,18 @@ grails.project.dependency.resolution = {
         runtime ":hibernate:$grailsVersion"
         runtime ":jquery:1.8.0"
         runtime ":resources:1.0"
+<<<<<<< HEAD
 
 		compile ":tooltip:0.7"
 		
+=======
+		runtime ":prototype:1.0"
+>>>>>>> origin/delivery
 		//compile  ":rendering:0.4.4"
 		//compile ":cache:1.0.1"		
 		//compile ":jquery-ui:1.8.24"
 		compile ":spring-security-core:1.2.7.3"
 		compile ":joda-time:1.4"
-		compile ":prototype:1.0"
 		compile ":pdf:0.6"
 		//compile ":quartz:1.0-RC6"
 		compile ":quartz2:2.1.6.2"
