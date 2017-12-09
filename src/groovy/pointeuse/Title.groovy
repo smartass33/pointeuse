63177174995bea60f7808846760875adfa859cf9
smartass33 <henri.martin@gmail.com>
2017-12-09 19:29:19 +0100

    add to index

---
+++ src/groovy/pointeuse/Title.groovy
@@ -0,0 +1,19 @@
+package pointeuse
+
+public enum Title {
+
+	//V,M,G,RTT,X
+	M('M'),
+	MME('MME'),
+	MLLE('MLLE')
+	
+	final String value
+    Title(String value){ this.value = value }
+    @Override
+    String toString(){ value }
+    String getKey() { name() }	
+
+	static Title valueOfName( String name ) {
+		values().find { it.value == name }
+	}
+}
